f0e77c475c87812360b28ab2c9c936be916ea824
Shifat Hossain <amithassan3229@gmail.com>
2020-11-09 15:32:17 +0600

    customize frontend page

---
+++ resources/views/layouts/frontend_layout/headerlink.blade.php
@@ -0,0 +1,32 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="description" content="multikart">
+<meta name="keywords" content="multikart">
+<meta name="author" content="multikart">
+<link rel="icon" href="{{asset('public')}}/frontend_asset/images/favicon/1.png" type="image/x-icon">
+<link rel="shortcut icon" href="{{asset('public')}}/frontend_asset/images/favicon/1.png" type="image/x-icon">
+
+<title>Sourcing Door - @yield('title')</title>
+
+<!--Google font-->
+<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+
+<!-- Icons -->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/fontawesome.css">
+
+<!--Slick slider css-->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/slick.css">
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/slick-theme.css">
+
+<!-- Animate icon -->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/animate.css">
+
+<!-- Themify icon -->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/themify-icons.css">
+
+<!-- Bootstrap css -->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/bootstrap.css">
+
+<!-- Theme css -->
+<link rel="stylesheet" type="text/css" href="{{asset('public')}}/frontend_asset/css/color1.css" media="screen" id="color">
